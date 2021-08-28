548a4ac6447a6a3c6dc4c9496124fe8acb9d1bfb
dante01yoon <bunggl@naver.com>
2021-08-28 21:12:11 +0900

    Update: toPos

---
+++ src/drag.js
@@ -0,0 +1,34 @@
+import { fromEvent, switchMap, takeUntil } from "rxjs";
+
+const $view = document.getElementById("carousel");
+const $container = $view.querySelector(".container");
+const PANEL_COUNT = $container.querySelectorAll(".panel").length;
+
+const SUPPORT_TOUCH = "ontouchstart" in window;
+
+const EVENTS = {
+  start: SUPPORT_TOUCH ? "touchstart" : "mousedown",
+  move: SUPPORT_TOUCH ? "touchmove" : "mousemove",
+  end: SUPPORT_TOUCH ? "touchend" : "mouseup",
+}
+
+function toPos($obs) {
+  return $obs
+    .pipe(
+      map(v => SUPPORT_TOUCH ? v.changedTouches[0].pageX : v.pageX),
+    );
+}
+
+const start$ = fromEvent($view, EVENTS.start)
+  .pipe(
+    toPos,
+  );
+const move$ = fromEvent($view, EVENTS.move)
+  .pipe(
+    toPos,
+  );
+const end$ = fromEvent($view, EVENTS.end);
+
+const drag$ = start$.pipe(
+  switchMap(start => move$.pipe(takeUntil(end$))),
+);
